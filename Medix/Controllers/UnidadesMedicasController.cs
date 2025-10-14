58bd974db9d1dd61bc2491aa9967620bdf01abbf
Arthur Thomas <athomasmariano@gmail.com>
2025-10-14 13:05:09 -0300

    style: ajusta organização e cores da página de registro

--- Medix/Controllers/UnidadesMedicasController.cs
+++ Medix/Controllers/UnidadesMedicasController.cs
@@ -55,8 +55,6 @@ namespace Medix.Controllers
         }
 
         // POST: UnidadesMedicas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Nome,CNPJ,Endereco,Telefone,EmailAdmin,Status,DataCadastro")] UnidadeMedica unidadeMedica)
@@ -91,8 +89,6 @@ namespace Medix.Controllers
         }
 
         // POST: UnidadesMedicas/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,CNPJ,Endereco,Telefone,EmailAdmin,Status,DataCadastro")] UnidadeMedica unidadeMedica)
